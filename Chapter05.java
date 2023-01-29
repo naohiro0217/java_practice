c6486b27d7a98c93e3a091fdf3ee83b5c98c2bb8
NaohiroFujiwara <112791193+naohiro0217@users.noreply.github.com>
2023-01-29 13:50:44 +0000

    java５章完了

---
+++ Chapter05.java
@@ -0,0 +1,25 @@
+
+public class Chapter05 {
+	public static void main(String[] args) {
+		long l = 9223372036854775807L;
+		System.out.println(l);
+		
+		double d = 1234567.89d;
+		System.out.println(d);
+		
+		boolean b = true;
+		System.out.println(b);
+		
+		Integer I = 5;
+		System.out.println(I);
+		
+		Float f = 0.123f;
+		System.out.println(f);
+		
+		Character c = 'あ';
+		System.out.println(c);
+		
+		int[] array = { 0, 1, 2, 3 };
+		System.out.println(array[3]);
+	}
+}
