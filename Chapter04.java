eb4748bbb589908674f47c240c8a0c8635cee7c2
NaohiroFujiwara <112791193+naohiro0217@users.noreply.github.com>
2023-01-29 13:50:19 +0000

    java４章完了

---
+++ Chapter04.java
@@ -0,0 +1,14 @@
+public class Chapter04 {
+	public static void main(String[] args) {
+		int lucky_number;
+		String str1;
+		str1 = "藤原直浩";
+		System .out .println (str1);
+
+		str1 = "ありがとう";
+		System.out.println(str1);
+
+		lucky_number = 11;
+		System.out.println(lucky_number);
+	}
+}
