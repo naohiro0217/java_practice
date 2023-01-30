654b39896989cf4b534845adfa3551db40096b47
NaohiroFujiwara <112791193+naohiro0217@users.noreply.github.com>
2023-01-30 05:56:05 +0000

    7章完了

---
+++ Chapter07/Chapter07.java
@@ -0,0 +1,14 @@
+public class Chapter07 {
+	public static void main(String[] args) {
+		int km = 7;
+		if (km <= 5) {
+			System.out.println("とても近いです");
+		} else if (km <= 10) {
+			System.out.println("近いです");
+		} else if (km <= 15) {
+			System.out.println("遠いです");
+		} else {
+			System.out.println("とても遠いです");
+		}
+	}
+}
