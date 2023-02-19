0078a1980db4b34dda8bb9b8bcb907b113f20d45
NaohiroFujiwara <112791193+naohiro0217@users.noreply.github.com>
2023-02-19 03:35:12 +0000

    javasurasura

---
+++ collection.java
@@ -0,0 +1,11 @@
+public class collection1 {
+	public static void main(String[] args) {
+		ArrayList<String> fruits;
+		fruits = new ArrayList();
+		
+		fruits.add("りんご");
+		fruits.add("みかん");
+		
+		fruuits.add(1, "ぶどう");
+	}
+}
